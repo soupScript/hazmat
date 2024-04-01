6495080b42f6a9b9c9cdf26c4465922a43bae179
soupScript <120066246+soupScript@users.noreply.github.com>
2024-04-01 21:31:32 +0100

    HAZMAT 0.11
    Rewrote it lmao and removed my shitty db handling lib instead i used dictionaries

--- hazrunner.py
+++ hazrunner.py
@@ -1,233 +1,58 @@
-import gdb
-
-def isint(valvarname):
-    try:
-      if valvarname == int(valvarname):
-         return True
-      else:
-         return False
-    except (ValueError, TypeError):
-      return False
-    
-def isstring(valvarname):
-    try:
-      if valvarname == string(valvarname):
-         return True
-      else:
-         return False
-    except (ValueError, TypeError):
-      return False
-    
-def isfloat(valvarname):
-    try:
-      if valvarname == float(valvarname):
-         return True
-      else:
-         return False
-    except (ValueError, TypeError):
-      return False
-    
-def ischar(valvarname):
-    try:
-      if valvarname[0] == valvarname:
-         return True
-      else:
-         return False
-    except (ValueError, TypeError):
-      return False
-
-
-    
-
-gdb.addt('prefuncs')
-gdb.addt('defuncs')
 variables = {}
+defuncs = {}
+prefuncs = {}
 
-gdb.insertvtt('#printlin', 'prefuncs', False)
-gdb.insertvtt(print, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(False, 'prefuncs', False)
+reader = 0
+fil = ""
 
-gdb.insertvtt('#getlin', 'prefuncs', False)
-gdb.insertvtt(input, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(True, 'prefuncs', False)
 
-gdb.insertvtt('#isint', 'prefuncs', False)
-gdb.insertvtt(isint, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(True, 'prefuncs', False)
+prefuncs.update({"#println": print})
+prefuncs.update({"#getln": input})
 
-gdb.insertvtt('#isstring', 'prefuncs', False)
-gdb.insertvtt(isstring, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(True, 'prefuncs', False)
+while fil != "end" and fil != "^C":
+    fil = input("")
+    if fil.strip() == "hazr -r":
+        print("Filename or path?: ", end="", flush=True)
+        fil = input("")
+        with open(fil, 'r') as f:
+            rfile = f.read()
+        while reader < len(rfile):
+            if rfile[reader] == "!":
+                s = rfile[reader: rfile.index(";", reader)]
+                if "=" in s:
+                    cvattr = s.split("=")
+                    variable_name = cvattr[0].strip()
+                    variable_value = cvattr[1].strip()
+                    if len(variable_value) > 1 and variable_value[0] == '"' and variable_value[-1] == '"':
+                        variable_value = variable_value[1:-1]
 
-gdb.insertvtt('#isfloat', 'prefuncs', False)
-gdb.insertvtt(isfloat, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(True, 'prefuncs', False)
+                    variables[variable_name] = variable_value
+            if rfile[reader] == "#":
 
-gdb.insertvtt('#ischar', 'prefuncs', False)
-gdb.insertvtt(ischar, 'prefuncs', False)
-gdb.insertvtt('manargs', 'prefuncs', False)
-gdb.insertvtt(1, 'prefuncs', False)
-gdb.insertvtt(True, 'prefuncs', False)
+                name = rfile[reader:rfile.index("(", reader)]
+                gargs = rfile[rfile.index('(', reader)+1:rfile.index(')', reader)]
 
-def gettype(value):
-            try:
-              if value[0] == '"':
-                if value[len(value) -1] == '"':
-                  return 'String'
-                else:
-                  return 'Raw'
-              elif isinstance(value, int):
-                return 'Int'
-              elif isinstance(value, float):
-                  return 'Float'
-              elif value[0] == '#':
-                testtgvt = value.split('(')
-                if testtgvt[1][0] == '(' and len(testtgvt[1] -1) == ')':
-                  return 'FuncRef'
-                else:
-                  return 'FuncKey'
-              elif value[0] == '!':
-                return 'OtherVar'
-              elif value[0] == '[':
-                if value[len(value) -1] == ']':
-                  return 'Array'
-                else:
-                  return 'Raw'
-              elif value[0] == '@':
-                if value[1] == '{' and value[len(value) -1] == '}':
-                  return 'Def'
-                else:
-                  return 'Raw'
-              else:
-                return 'Raw'
-              #end
-            except (IndexError, ValueError, TypeError):
-              pass
+                gargsarr = gargs.split(',')
+                checkfstringc = 0
+                while checkfstringc < len(gargsarr):
+                    if gargsarr[checkfstringc][0] == '"' and gargsarr[checkfstringc][len(gargsarr[checkfstringc])-1] == '"':
+                        gargsarr[checkfstringc] = gargsarr[checkfstringc][1:len(gargsarr)-2]
+                        checkfstringc+=1
+                    elif gargsarr[checkfstringc][0] == '!':
+                        gargsarr[checkfstringc] = variables.get(gargsarr[checkfstringc])
+                        checkfstringc+=1
+
+                if name in prefuncs:
+                    prefuncs[name](*gargsarr)
+
+
+            reader+=1
+        print("\n\n=====FINISHED=====\n\n")
 
 
 
 
-print('type help to get list of commands')
-functionkey = '#'
-variablekey = '!'
-definefunctionkey = '@'
-keywordkey = '$'
-codea = []
-#TYPE LIST!!!!!!!!!!!!!!!!!
-types = ['OtherVar', 'String', 'Int', 'Float', 'Raw', 'Array', 'FuncRef', 'FuncKey', 'Def']
 
-
-try:
-  while True:
-    print('hazrunner>', end='', flush=True)
-    command = input('')
-    if command == 'help':
-      print('hazr COMMANDS:')
-      print('-r: runs a file. Type hazr -r, then you will be prompted to give either a file path or name\n (Depending whether or not the haz file is in the same dir \n as the runner, which it should be, for convenience. )')
-      print('\n')
-      print('-sc: shows the contents of a file. Type hazr -sc')
-    elif command == 'hazr -sc':
-      
-      
-      print('hazrunner>filename/path?> ', end='', flush=True)
-      command = input('')
-      try:
-        with open(command, 'r') as f:
-          code = f.read()
-        print(code)
-        
-      except:
-        print('FILE NOT FOUND ERROR!')
-    elif command == 'hazr -r':
-    
-      print('hazrunner>filename/path?> ', end='', flush=True)
-      command = input('')
-      with open(command, 'r') as f:
-        code = f.read()
-      counter = 0
-      letter = code[counter]
-      while counter < len(code):
-        #handle variable declarations
-
-        
-        if letter == variablekey:
-          string = code[counter:code.index(';', counter)]
-          if '=' in string:
-                        var_name, var_value = string.split('=')
-                        variables[var_name.strip()] = var_value.strip()
-        #handle functions
-        elif letter == functionkey:
-          openingparentheses = code.index('(', counter)
-          funcname = code[counter: openingparentheses]
-
-          if funcname in gdb.db[gdb.db.index('prefuncs'):gdb.db.index('endtab;', gdb.db.index('prefuncs'))]:
-            index = gdb.db.index(funcname) + 1
-            actualfunction = gdb.db[index] 
-            args = []
-
-            argam = 0
-            subcounter = code.index('(', counter)
-            subletter = code[subcounter]
-            arg_string = code[subcounter + 1: code.index(')', counter)]
-            ends = len(arg_string)
-            args = arg_string.split(',')
-            subcounter2 = 0          
-            subletter2 =  args[subcounter2]
-            string2 = code[subcounter2 + 1:code.index(')', counter)]
-            #add to after *1
-            
-            #*1
-            
-            
-
-
-            for i, arg in enumerate(args):
-              if gettype(arg) == 'OtherVar':
-                  var_name = arg.strip()  
-                  if var_name in variables:
-                      args[i] = variables[var_name]
-                  else:
-                      print(f'ERROR {var_name} NOT FOUND!')
-
-              if gettype(arg) == 'FuncRef':
-                if gdb.db[gdb.db.index(arg) + 5] == True:
-                    argsoa = arg.split('(')
-                    two = argsoa[1]
-                    argsoa = two.split(',')
-                    argsoa[0] = argsoa[0].split('(')
-                    argsoa[len(argsoa) - 1].split(')')
-                    try:
-                        var1234 = arg(*args)
-                    except Exception as e:
-                        print(f"Error: {e}")
-                        args[i] = var1234
-                            
-
-            actualfunction(*args)
-
-          elif funcname in gdb.db[gdb.db.index('defuncs'):gdb.db.index('endtab;', gdb.db.index('defuncs'))]:
-            pass
-          else:
-            print(f'FUNCTION {funcname} NOT FOUND!')
-          
-        codea.append(letter)
-        counter += 1
-        if counter < len(code):  
-          letter = code[counter]
-    
-
-except KeyboardInterrupt:
-  pass
 
 
 
