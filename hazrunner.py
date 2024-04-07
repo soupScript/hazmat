0e76cb77197a1161bd91662ed10f59bdd6a1cccc
soupScript <120066246+soupScript@users.noreply.github.com>
2024-04-07 19:14:35 +0000

    Fixed bc replit being wierd
    replit was wierd but i saved myself bc i had a backup file lmao

--- hazrunner.py
+++ hazrunner.py
@@ -9,12 +9,20 @@ fil = ""
 
 def printnoend(args):
   print(args, end="")
+def saymoo(ms, os):
+    for i in range(ms):
+        print("M", end="", flush=True)
+    for i in range(os):
+        print("O", end="", flush=True)
+    print("")
+
   
 
 
 prefuncs.update({"#println": print})
 prefuncs.update({"#getln": input})
 prefuncs.update({"#print":printnoend})
+prefuncs.update({"#moo":saymoo})
 
 def gtype(val):
     if len(val) > 1 and val[0] == '"' and val[-1] == '"':
@@ -62,35 +70,21 @@ while fil != "end" and fil != "^C":
             
         
         while reader < len(rfile):
-            if rfile[reader] == "?":
-                statement = rfile[reader+1:rfile.index("{", reader)]
-                statearr = statement.split("~")
-                statestr = ""
-                counter = 0
-                while counter < len(statearr):
-                    if statearr[counter] != statearr[1]:
-                        statearr[counter] = gtype(statearr[counter])
-                        statestr = statestr+ f"{statearr[counter]}"
-                        counter+=1
-                    else:
-                         statestr = statestr+ f"{statearr[counter]}"
-                         counter+=1
-                if eval(statestr):
-                    do = rfile[rfile.index("{", reader)+1:rfile.index("|", reader)]
-                    if do in prefuncs:
-                        argsff = rfile[rfile.index("|", reader)+1:rfile.index("}", reader)].split(",")
-                        checkfstringc = 0
-                        while checkfstringc < len(argsff):
-                            argsff[checkfstringc] = gtype(argsff[checkfstringc])
-                            checkfstringc+=1
-                        prefuncs.get(do)(*argsff)
-                    else:
-                        pass
-                        #IF DO IN DEFUNCS
-                    
-                else:
-                    if "else{" in rfile:
-                        finish("else{")
+            try:
+                if rfile[reader] == "?":
+                    statement = rfile[reader+1:rfile.index("{", reader)]
+                    statearr = statement.split("~")
+                    statestr = ""
+                    counter = 0
+                    while counter < len(statearr):
+                        if statearr[counter] != statearr[1]:
+                            statearr[counter] = gtype(statearr[counter])
+                            statestr = statestr+ f"{statearr[counter]}"
+                            counter+=1
+                        else:
+                            statestr = statestr+ f"{statearr[counter]}"
+                            counter+=1
+                    if eval(statestr):
                         do = rfile[rfile.index("{", reader)+1:rfile.index("|", reader)]
                         if do in prefuncs:
                             argsff = rfile[rfile.index("|", reader)+1:rfile.index("}", reader)].split(",")
@@ -100,69 +94,81 @@ while fil != "end" and fil != "^C":
                                 checkfstringc+=1
                             prefuncs.get(do)(*argsff)
                         else:
-                            pass#IF DO IN DEFUNCS
+                            pass
+                            #IF DO IN DEFUNCS
+                        
+                    else:
+                        if "else{" in rfile:
+                            finish("else{")
+                            do = rfile[rfile.index("{", reader)+1:rfile.index("|", reader)]
+                            if do in prefuncs:
+                                argsff = rfile[rfile.index("|", reader)+1:rfile.index("}", reader)].split(",")
+                                checkfstringc = 0
+                                while checkfstringc < len(argsff):
+                                    argsff[checkfstringc] = gtype(argsff[checkfstringc])
+                                    checkfstringc+=1
+                                prefuncs.get(do)(*argsff)
+                            else:
+                                pass#IF DO IN DEFUNCS
 
-
-                
-
-                finish("};")
-            
-            if rfile[reader] == "!":
-                s = rfile[reader: rfile.index(";", reader)]
-                if "=" in s:
-                    cvattr = s.split("=")
-                    variable_name = cvattr[0].strip()
-                    variable_value = cvattr[1].strip()
-                    variable_value = gtype(variable_value)
 
                     
 
-
-                    if variable_name not in variables:
-                         variables.update({variable_name:variable_value})
-                    else:
-                        variables[variable_name]=variable_value
-                    finish(";")
+                    finish("};")
                 
+                if rfile[reader] == "!":
+                    s = rfile[reader: rfile.index(";", reader)]
+                    if "=" in s:
+                        cvattr = s.split("=")
+                        variable_name = cvattr[0].strip()
+                        variable_value = cvattr[1].strip()
+                        variable_value = gtype(variable_value)
+
                         
+
+
+                        if variable_name not in variables:
+                            variables.update({variable_name:variable_value})
+                        else:
+                            variables[variable_name]=variable_value
+                        finish(";")  
+
+                if rfile[reader] == "#":
+
+                    name = rfile[reader:rfile.index("(", reader)]
+                    gargs = rfile[rfile.index('(', reader)+1:rfile.index(')', reader)]
+
+                    gargsarr = gargs.split(',')
+                    checkfstringc = 0
+                    while checkfstringc < len(gargsarr):
+                        gargsarr[checkfstringc] = gtype(gargsarr[checkfstringc])
+                        checkfstringc+=1
+
+                    if name in prefuncs:
+                        prefuncs[name](*gargsarr)
+                    finish(";")
+            except Exception as e:
+                exception_type = type(e).__name__
                 
+                print("\n")
+                magicnumber=18
+                print(rfile[reader:reader+magicnumber])
+                for i in range(magicnumber):
+                    if i>len(rfile):
+                        break;
+                    else:
+                        print("^", end="", flush=True)
+                print("\n")
+                print(f"{exception_type} error at char {reader}.")
+                break;
                 
-                
-
-            
-            if rfile[reader] == "#":
-
-                name = rfile[reader:rfile.index("(", reader)]
-                gargs = rfile[rfile.index('(', reader)+1:rfile.index(')', reader)]
-
-                gargsarr = gargs.split(',')
-                checkfstringc = 0
-                while checkfstringc < len(gargsarr):
-                    gargsarr[checkfstringc] = gtype(gargsarr[checkfstringc])
-                    checkfstringc+=1
-
-                if name in prefuncs:
-                    prefuncs[name](*gargsarr)
-                finish(";")
 
 
             reader+=1
+           
         print("\n=====FINISHED=====\n")
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-    
-
-      
